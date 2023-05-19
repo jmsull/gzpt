ridiculous ZA interpolation overflow bug

gzpt/bb.py
@@ -31,7 +31,7 @@ def PBB(k,params,nmax,wantGrad=False):
     elif(nmax==2):
         A0,R,R1h,R1sq,R12 = params
         R2h = R1h/(np.sqrt(2)*R12) #R12 is R1h/R2h/sqrt(2) and is forced to be > 1
-        if(R12<=.99) : raise ValueError("R12 must be greater than 1 for physical values.")
+        if(R12<=.99 and R12!=0.) : raise ValueError("R12 must be greater than 1 for physical values.")
 
     def F_comp(k,R):
         """For now only Lorentzian compensation"""

gzpt/gzpt.py
@@ -49,6 +49,7 @@ class hzpt:
         if(self.config):
             self.rxi = np.logspace(-1,3,4000) #matching up with the SBT in zel
             xiza = self.cleft.compute_xi_real(self.rxi)
+            assert np.all(~np.isnan(xiza))
             self.Xi_zel = loginterp(self.rxi,xiza) #callable
         else:
             warnings.warn("Configuration space models will not work if config flag is off!",
@@ -58,4 +59,5 @@ class hzpt:
         self.cleft.make_ptable(Dz=Dz)
         self.P_zel = loginterp(*self.cleft.pktable.T)
         if(self.config):
-            self.Xi_zel = loginterp(self.rxi,self.cleft.compute_xi_real(self.rxi))
+            xiza = self.cleft.compute_xi_real(self.rxi)
+            self.Xi_zel = loginterp(self.rxi,xiza)

gzpt/zel.py
@@ -55,7 +55,8 @@ def loginterp(x, y, yint = None, side = "both", lorder = 9, rorder = 9, lp = 1, 
     xl = np.logspace(-12, np.log10(x[l]), 10**5)
     xr = np.logspace(np.log10(x[r]), 12., 10**5)
     yl = y[l]*(xl/x[l])**lneff
-    yr = y[r]*(xr/x[r])**rneff
+    yr = y[r]*(xr/x[r])**np.minimum(rneff,10.0)
+    #print(yr[-10:],rneff,y[r],x[r],xr[-10:])
 
     #put it together
     xint = x[l+1:r].copy()